33ed039828412fd0df66ff11256fb72384cb8562
nguyenha0081988-lab <nguyenha0081988@gmail.com>
2025-10-06 16:18:36 +0700

    start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-# Tăng thời gian chờ lên 120 giây
-gunicorn api.app:app -t 120
+# Tăng thời gian chờ (timeout) lên 300 giây và chỉ định 4 worker ổn định
+gunicorn api.app:app -t 300 -w 4
